9ab0d19cb59fa8293d8a2f03aaff58f3c7f6cfa1
Dragan Krstic <dragank@gmail.com>
2022-09-23 14:46:19 +0200

    CV in index  page.

--- pages/index.js
+++ pages/index.js
@@ -4,20 +4,19 @@ import Footer from '@components/Footer'
 
 export default function Home() {
   return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-
-      <Footer />
-    </div>
+<iframe src="/CV_Dragan_Krstic.pdf"
+  style="
+    position: fixed;
+    top: 0px;
+    bottom: 0px;
+    right: 0px;
+    width: 100%;
+    border: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    z-index: 999999;
+    height: 100%;"
+    >
   )
 }
