fc19531468f0dcab0d1884dfe06d8d53aec7280f
Dragan Krstic <dragank@gmail.com>
2023-09-21 15:42:08 +0200

    Resume swap.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Safe from "react-safe"
 export default function Home() {
   return (
     <Safe.script>{
-      'window.location.replace("/CV_Dragan_Krstic.pdf");'
+      'window.location.replace("/Dragan_Krstic_resume.pdf");'
       }
     </Safe.script>
   )
